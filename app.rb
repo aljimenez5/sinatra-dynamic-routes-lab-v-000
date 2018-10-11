af62a0a3c4b491d54a0cddcc2a3c39b5bcccc208
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-10-11 00:19:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
+    @operation = params[:operation].to_sym
     @first_number = params[:number1]
     @second_number = params[:number2]
     @operations = {:add => "+", :substract => "-", :multiply = "*", :divide = "/"}

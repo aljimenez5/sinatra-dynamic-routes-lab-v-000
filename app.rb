3d43a3433c852379bf44ae5d81098a77a39681a8
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-10-11 00:09:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,9 @@ class App < Sinatra::Base
   
   get '/:operation/:number1/:number2' do
     @operation_string = params[:operation]
-    add = +
-    substract = -
+    add = "+"
+    substract = "-"
+    multiply = "*"
+    divide = "/"
   
 end
